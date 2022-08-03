0c8900eb4bab5d275bb12c75c393e438743de42b
moonkhan1 <ayxanhesenov3105@gmail.com>
2022-08-03 17:39:21 +0400

    GameManager created and singleton principle implemented

---
+++ UnityProject1/Assets/GameFolder/Scripts/Concrete/Controllers/FinishFloorController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityProject1.Controllers{
+public class FinishFloorController : MonoBehaviour
+{
+    [SerializeField] GameObject _finishLights;
+    [SerializeField] GameObject _finishFireWorks;
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        PlayerController player = other.collider.GetComponent<PlayerController>();  
+
+        if(player == null) return;
+
+        //Eger obyekt(player) platformaya yuxaridan deyerse qalib olur
+        if(other.GetContact(0).normal.y == -1)
+        {
+            // Fireworksler ve Isiqlar yanir
+            // Debug.Log("asdas");
+            _finishFireWorks.gameObject.SetActive(true);
+            _finishLights.gameObject.SetActive(true);
+        }
+        else
+        {
+            // Eger player platformanin sagina,soluna, asagisina deyerese Game Over olur
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+    }
+
+}
+}
